d32bdf60dc6a3e0235f03f409b4c3fcf01b5d627
tanqiyuan123 <73412554+tanqiyuan123@users.noreply.github.com>
2021-01-05 23:24:20 +0800

    Fix -> The Quiz component isn't rendering a single div with the text Quiz.

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,7 +8,7 @@ class Quiz extends Component {
     }
     render() {
         return (
-            <div class='QuizQuestion'>{quizData.quiz_questions[0].instruction_text}</div>
+            <div>Quiz</div>
         );
     }
 }
