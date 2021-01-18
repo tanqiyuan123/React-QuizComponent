fe9305a6c5f025b858045d456e1b4f3f758b2f18
tanqiyuan123 <73412554+tanqiyuan123@users.noreply.github.com>
2021-01-18 23:16:09 +0800

    Module5: Displaying Quiz Questions and Handling Events

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,13 +8,20 @@ class Quiz extends Component {
         super(props)
         this.state = {quiz_position: 1}
     }
+
+    showNextQuestion() {
+        this.setState ({
+            quiz_position: this.state.quiz_position + 1
+        })
+    }
+
     render() {
         const isQuizEnd = this.state.quiz_position - 1 == quizData.quiz_questions.length
         return (
             <div>
                 {/* <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div> */}
                 {isQuizEnd? (<QuizEnd />) : 
-                (<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />)
+                (<QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />)
                 }
             </div>
         );
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,17 +3,26 @@ import QuizQuestionButton from './QuizQuestionButton'
 
 class QuizQuestion extends Component {
 
+    handleClick(buttonText) {
+        if (buttonText === this.props.quiz_question.answer) {
+            this.props.showNextQuestionHandler()
+        }
+    }
     render() {
         return (
             <main>
                 <section>
-                <p>{this.props.quiz_question.instruction_text}</p>
+                    <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
                 <section className="buttons">
-                <ul>
-                    {/* <li>{this.props.quiz_question.answer_options[0]}</li> */}
-                    <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
-                </ul>
+                    <ul>
+                        {/* <li>{this.props.quiz_question.answer_options[0]}</li> */}
+                        {/* <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} /> */}
+
+                        {this.props.quiz_question.answer_options.map((answer_option, index) => {
+                            return <QuizQuestionButton clickHandler={this.handleClick.bind(this)} key={index} button_text={answer_option} />
+                        })}
+                    </ul>
                 </section>
             </main>
         );
